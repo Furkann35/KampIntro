d7ca6870eee40d088cac57eaf24aa4efbde1b44f
Furkann35 <yucelfurkan124@gmail.com>
2021-04-20 03:44:14 +0300

    References Types

---
+++ Generics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Ankara");
+            Console.WriteLine(sehirler.Count);
+
+
+
+            Mylist<string> sehirler2 = new Mylist<string>();
+            sehirler2.Add("Ankara");
+            Console.WriteLine(sehirler2.Count);
+        }
+    }
+
+    class Mylist<T> //Generic Class
+    {
+        T[] _array;
+        T[] _tempArray;
+         public Mylist()
+        {
+            _array = new T[0];
+
+        }
+
+
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+             }
+            _array[_array.Length - 1] = item;
+        }
+
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+
+    }
+
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //Method injectip
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService) 
+        {
+            //Başvuru bilgilerini değerlendirme
+            //
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+
+    }
+}
