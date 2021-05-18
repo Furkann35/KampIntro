4c091bb87180311233aa6a39e3674a8c96d98c0c
Furkann35 <yucelfurkan124@gmail.com>
2021-05-18 10:34:58 +0300

    EntityFrameworkDemo

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+    } 
+}
